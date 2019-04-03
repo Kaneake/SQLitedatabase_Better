f2aa95494b461751272c90b5aa1e46a75e163174
kanykeinurmambetova <kanykei2610@gmail.com>
2019-04-03 12:27:38 -0600

    Initial commit

---
+++ app/src/main/java/com/example/kanykeinurmambetova/sqlitedatabase/WhenClickItem.java
@@ -0,0 +1,13 @@
+package com.example.kanykeinurmambetova.sqlitedatabase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WhenClickItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_when_click_item);
+    }
+}
